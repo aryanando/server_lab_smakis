0f656144c9fff62d93a5ff30b6752b3b8932f781
aryanando <aryanando232@gmail.com>
2022-06-23 12:54:54 +0700

    Template Untuk Remote Mikrotik

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        shell_exec('SSH admin@10.10.10.1 "ip hotspot user add name=Nandooi password=1234 server=all"');
+        shell_exec('SSH admin@202.6.236.214 -p 1023 "ip hotspot user add name=Nandoi password=1234 server=all"');
         return view('welcome_message');
     }
 }
